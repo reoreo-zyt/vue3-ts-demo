f100bb070685f362bf477aa3e8c4c2c84cc6549e
reoreo <768119359@qq.com>
2022-11-09 18:36:33 +0800

    build: prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,31 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential",
-        "plugin:@typescript-eslint/recommended"
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-essential",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/ban-types": [
+      "error",
+      {
+        extendDefaults: true,
+        types: {
+          "{}": false,
+        },
+      },
     ],
-    "parser": "vue-eslint-parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "@typescript-eslint/ban-types": [
-            "error",
-            {
-                "extendDefaults": true,
-                "types": {
-                    "{}": false
-                }
-            }
-        ]
-    }
-}
+  },
+};
--- README.md
+++ README.md
@@ -4,15 +4,15 @@
 
 行尾序列编码不一致导致的问题
 
-在使用工程vue-cli建立工程时，会把文件的行尾序列一致为LF
+在使用工程 vue-cli 建立工程时，会把文件的行尾序列一致为 LF
 
-windows系统使用git时，在提交代码时会把CRLF更改为LF，在拉取代码时，会把LF更改为CRLF
+windows 系统使用 git 时，在提交代码时会把 CRLF 更改为 LF，在拉取代码时，会把 LF 更改为 CRLF
 
-windows端行尾序列为CRLF。MAC端行尾序列为LF
+windows 端行尾序列为 CRLF。MAC 端行尾序列为 LF
 
 ## 1. 代码提交规范
 
-br: 此项特别针对bug号，用于向测试反馈bug列表的bug修改情况
+br: 此项特别针对 bug 号，用于向测试反馈 bug 列表的 bug 修改情况
 
 feat：新功能（feature）
 
@@ -22,22 +22,22 @@ docs：文档（documentation）
 
 style： 格式（不影响代码运行的变动）
 
-refactor：重构（即不是新增功能，也不是修改bug的代码变动）
+refactor：重构（即不是新增功能，也不是修改 bug 的代码变动）
 
 chore：其他的小改动. 一般为仅仅一两行的改动, 或者连续几次提交的小改动属于这种
 
-revert：撤销之前的commit
+revert：撤销之前的 commit
 
 upgrade：升级改造
 
-bugfix：修补bug
+bugfix：修补 bug
 
 optimize：优化
 
-perf: Performance的缩写, 提升代码性能
+perf: Performance 的缩写, 提升代码性能
 
 test：新增测试用例或是更新现有测试
 
-ci:主要目的是修改项目继续完成集成流程(例如Travis，Jenkins，GitLab CI,Circle)的提交
+ci:主要目的是修改项目继续完成集成流程(例如 Travis，Jenkins，GitLab CI,Circle)的提交
 
-build: 主要目的是修改项目构建系统(例如glup，webpack，rollup的配置等)的提交
+build: 主要目的是修改项目构建系统(例如 glup，webpack，rollup 的配置等)的提交
