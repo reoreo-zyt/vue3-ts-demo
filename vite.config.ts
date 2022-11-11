51bd664b72f73faa4cbc8204b32c6a586175cbeb
reoreo <768119359@qq.com>
2022-11-11 18:59:27 +0800

    build: scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    // Vite 路径别名配置
+    alias: {
+      '@': path.resolve('./src'),
+    },
+  },
+  // 将 css 变量加载到全局，在 style 标签中可以使用
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/styles/constant.scss";',
+      },
+    },
+  },
   plugins: [vue()],
 })
