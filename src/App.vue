7c3801b3c38ac233cad8805677e77c3f28630597
reoreo <57691895+reoreo-zyt@users.noreply.github.com>
2022-11-26 14:13:51 +0800

    fix: bug

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,7 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-// 验证 scss
-// import HelloWorld from './components/HelloWorld.vue'
-import Cesium from './components/CesiumTest.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <!-- <HelloWorld msg="Hello Scss" /> -->
-  <div class="app">
-    <Cesium />
-  </div>
+  <div class="app"></div>
 </template>
 
 <style scoped>
