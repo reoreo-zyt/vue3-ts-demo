62237ea4346b5a870587387d1589749017ebcf06
reoreo <57691895+reoreo-zyt@users.noreply.github.com>
2022-11-27 23:35:54 +0800

    build: remove cesium

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
-import cesium from 'vite-plugin-cesium'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,5 +18,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue(), cesium()],
+  plugins: [vue()],
 })
