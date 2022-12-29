8fe16036af6a4e852d65caa85d21c172b3be40cf
reoreo <768119359@qq.com>
2022-12-29 10:42:21 +0800

    chore: remove

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,5 @@ import './style.css'
 // 在 .vue 文件中使用公共类的样式
 import '@/styles/index.scss'
 import App from './App.vue'
-import naiveui from './plugins/naiveui'
 
-createApp(App).use(naiveui).mount('#app')
+createApp(App).mount('#app')
