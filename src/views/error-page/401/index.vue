4f7dddb331b6281a158639b87a6f2ec602098874
reoreo-zyt <768119359@qq.com>
2023-10-16 11:24:40 +0800

    chore

--- src/views/error-page/401/index.vue
+++ src/views/error-page/401/index.vue
@@ -38,7 +38,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import errGif from '@/assets/401_images/401.gif'
 import { reactive, toRefs } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
