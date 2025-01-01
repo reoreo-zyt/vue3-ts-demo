35bf425e107f053d2344b786e95640cff68d08b0
reoreo-zyt <768119359@qq.com>
2025-01-01 15:26:19 +0800

    :construction: construction: 引入vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store'; // 创建vue实例
+import router from './router';
 
 const app = createApp(App); // 挂载pinia
 
 app.use(store); // 挂载实例
+app.use(router);
+
 app.mount('#app');
---
+++ src/router/index.ts
@@ -0,0 +1,35 @@
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+} from 'vue-router';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/login',
+    name: 'Login',
+    meta: {
+      title: '登录',
+      keepAlive: true,
+      requireAuth: false,
+    },
+    component: () => import('@/pages/login.vue'),
+  },
+  {
+    path: '/',
+    name: 'Index',
+    meta: {
+      title: '首页',
+      keepAlive: true,
+      requireAuth: true,
+    },
+    component: () => import('@/pages/index.vue'),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
