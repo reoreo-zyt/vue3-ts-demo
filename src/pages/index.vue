eb6903bfb6dea4bbfe5b0383ec52f8857d9a0eb7
reoreo-zyt <768119359@qq.com>
2025-01-01 15:40:53 +0800

    :refactor: feat: 引入scss

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>index.vue</div>
+  <div class="page-main">
+    <div class="page-main-item">index.vue</div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+$red: red;
+
+.page-main {
+  .page-main-item {
+    color: $red;
+  }
+}
+</style>
